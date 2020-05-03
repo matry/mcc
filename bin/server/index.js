4def4251f61acd6936fe7b46aaee452c7f23c41c
Daniel Vaughn <daniel.vaughn@whiteops.com>
2020-05-03 18:52:55 -0400

    kills child processes on sigint

--- bin/server/index.js
+++ bin/server/index.js
@@ -8,6 +8,13 @@ webpackDevServer.stdout.on('data', (data) => console.log(`stdout: ${data}`))
 webpackDevServer.stderr.on('data', (data) => console.error(`stderr: ${data}`))
 webpackDevServer.on('close', (code) => console.log(`child process exited with code ${code}`))
 
-watcher('src', () => {
+const watchProcess = watcher('src', () => {
   project('src', 'bin/client')
+})
+
+process.on('SIGINT', () => {
+  webpackDevServer.kill()
+  watchProcess.close().then(() => {
+    process.exit()
+  })
 })
--- bin/server/watcher.js
+++ bin/server/watcher.js
@@ -3,6 +3,8 @@ const chokidar = require('chokidar')
 const watcher = (srcDir, callback) => {
   const watchProcess = chokidar.watch(srcDir)
   watchProcess.on('all', callback)
+
+  return watchProcess
 }
 
 module.exports = watcher
