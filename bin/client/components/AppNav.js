69c488b56ba0d81f9dfa8e4dbd8c9bc6465f03cd
Daniel Vaughn <daniel.vaughn@whiteops.com>
2020-05-03 22:13:43 -0400

    styles nav items

--- bin/client/components/AppNav.js
+++ bin/client/components/AppNav.js
@@ -1,39 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from '@emotion/styled'
+import { ExpandLess, ExpandMore } from '@material-ui/icons'
 import { spacing } from '../theme'
+
+const StyledH3 = styled.h3`
+  display: flex;
+  align-items: center;
+  font-weight: 500;
+  font-size: 12px;
+  text-transform: uppercase;
+  cursor: pointer;
+`
 
 const StyledNav = styled.nav`
   padding: ${spacing(1)}px;
 `
 
+const StyledList = styled.ul`
+  list-style: none;
+  padding-left: 0px;
+  margin-bottom: 8px;
+`
+
+const StyledItem = styled.li`
+  padding-left: 20px;
+  font-size: 14px;
+  font-weight: 500;
+`
+
 const AppNav = ({ navTitles, isOpen, onClose, selectedTitle, onSelect }) => {
-  const navKeys = Object.keys(navTitles)
+  const tokenTitles = navTitles['Tokens']
+  const componentTitles = navTitles['Components']
+  const [tokensOpen, setTokensOpen] = useState(true)
+  const [componentsOpen, setComponentsOpen] = useState(true)
 
   return (
     <StyledNav>
-      {navKeys.map((key, i) => {
-        const titles = navTitles[key]
+      <StyledH3 onClick={() => setTokensOpen(!tokensOpen)}>
+        {tokensOpen ? <ExpandLess fontSize="small" /> : <ExpandMore fontSize="small" />}
+        Tokens
+      </StyledH3>
+      {!tokensOpen ? null : (
+        <StyledList>
+          {tokenTitles.map((item) => (
+            <StyledItem
+              style={{ color: item === selectedTitle ? '#007bff' : '#000' }}
+              key={item}
+              onClick={() => {
+                onSelect(item)
+                onClose()
+              }}
+            >
+              {item}
+            </StyledItem>
+          ))}
+        </StyledList>
+      )}
 
-        return (
-          <React.Fragment key={key}>
-            <h3>{key}</h3>
-            <ul>
-              {titles.map((item) => (
-                <li
-                  style={{ color: item === selectedTitle ? '#007bff' : '#000' }}
-                  key={`${key}_${item}`}
-                  onClick={() => {
-                    onSelect(item)
-                    onClose()
-                  }}
-                >
-                  {item}
-                </li>
-              ))}
-            </ul>
-          </React.Fragment>
-        )
-      })}
+      <StyledH3 onClick={() => setComponentsOpen(!componentsOpen)}>
+        {componentsOpen ? <ExpandLess fontSize="small" /> : <ExpandMore fontSize="small" />}
+        Components
+      </StyledH3>
+      {!componentsOpen ? null : (
+        <StyledList>
+          {componentTitles.map((item) => (
+            <StyledItem
+              style={{ color: item === selectedTitle ? '#007bff' : '#000' }}
+              key={item}
+              onClick={() => {
+                onSelect(item)
+                onClose()
+              }}
+            >
+              {item}
+            </StyledItem>
+          ))}
+        </StyledList>
+      )}
     </StyledNav>
   )
 }
